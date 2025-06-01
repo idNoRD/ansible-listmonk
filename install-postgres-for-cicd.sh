f4b28bfd7f790d098dcccdfe52d531c42a8d35df
idNoRD <idNoRD@users.noreply.github.com>
2025-06-01 00:05:03 -0700

    🐎 ci(ci): skip md5 and use trust from localhost in cicd
    skip md5 and use trust from localhost in cicd

--- install-postgres-for-cicd.sh
+++ install-postgres-for-cicd.sh
@@ -2,22 +2,21 @@
 set -e
 
 DB_NAME="listmonkdb"
-DB_PASS="S0meStr0ngP@ssword"
+#DB_PASS="S0meStr0ngP@ssword"
 
 dnf install -y postgresql-server
 postgresql-setup --initdb
 
 PG_HBA="/var/lib/pgsql/data/pg_hba.conf"
 sed -i -E \
-  -e 's/^(local\s+all\s+all\s+)(peer|ident)/\1md5/' \
-  -e 's/^(host\s+all\s+all\s+127\.0\.0\.1\/32\s+)\w+/\1md5/' \
-  -e 's/^(host\s+all\s+all\s+::1\/128\s+)\w+/\1md5/' \
+  -e 's/^(local\s+all\s+all\s+)(peer|ident|md5)/\1trust/' \
+  -e 's/^(host\s+all\s+all\s+127\.0\.0\.1\/32\s+)\w+/\1trust/' \
+  -e 's/^(host\s+all\s+all\s+::1\/128\s+)\w+/\1trust/' \
   "$PG_HBA"
 
 systemctl enable --now postgresql
 
-export PGPASSWORD="$DB_PASS"
-createdb -U postgres $DB_NAME
-psql -U postgres -c "ALTER USER postgres WITH PASSWORD '$DB_PASS';"
+#createdb -U postgres $DB_NAME
+#psql -U postgres -c "ALTER USER postgres WITH PASSWORD '$DB_PASS';"
 
 echo "✅ PostgreSQL installed and database '$DB_NAME' created with password auth."
