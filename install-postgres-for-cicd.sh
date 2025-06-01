bc6d8cb3a248232f77f5098dae7cd72d26877549
idNoRD <idNoRD@users.noreply.github.com>
2025-05-31 23:17:22 -0700

    🐎 ci(ci): Remove sudo from install-postgres script
    Container doesn't have sudo

--- install-postgres-for-cicd.sh
+++ install-postgres-for-cicd.sh
@@ -3,10 +3,10 @@ set -e
 
 DB_NAME="listmonkdb"
 
-sudo dnf install -y postgresql-server
-sudo postgresql-setup --initdb
-sudo systemctl enable --now postgresql
+dnf install -y postgresql-server
+postgresql-setup --initdb
+systemctl enable --now postgresql
 # Create a DB using the 'postgres' OS and DB user
-sudo -u postgres createdb $DB_NAME
+su - postgres -c "createdb $DB_NAME"
 
 echo "✅ PostgreSQL installed and database '$DB_NAME' created for user postgres"
